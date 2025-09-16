4626e9bb5de64472526138575f6ff5f0804c3243
Arthur-Els <arthurelshaddai.p@gmail.com>
2025-09-16 20:06:53 +0700

    pindah folder

---
+++ script.js
@@ -0,0 +1,16 @@
+  // Observer untuk animasi card
+  const cardObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target); // supaya animasi hanya sekali
+      }
+    });
+  }, {
+    threshold: 0.4
+  });
+
+  // Observe semua card
+  document.querySelectorAll(".card").forEach(card => {
+    cardObserver.observe(card);
+  });
